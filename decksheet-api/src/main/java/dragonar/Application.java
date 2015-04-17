98ee03a43eef2a0f66c978eca2d616a76c38d8f1
KojiTakahara <plateau1205@gmail.com>
2015-04-17 23:22:49 +0900

    change spring boot api

--- decksheet-api/src/main/java/dragonar/Application.java
+++ decksheet-api/src/main/java/dragonar/Application.java
@@ -1,65 +1,12 @@
 package dragonar;
 
-import org.apache.catalina.connector.Connector;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.web.HttpMessageConverters;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
-import org.springframework.boot.context.embedded.tomcat.TomcatConnectorCustomizer;
-import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.StringHttpMessageConverter;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.filter.CharacterEncodingFilter;
-
-import javax.sql.DataSource;
-import java.nio.charset.Charset;
-import java.util.Arrays;
 
 @SpringBootApplication
-public class Application extends SpringBootServletInitializer {
+public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(Application.class);
-    }
-
-    @Bean
-    public CharacterEncodingFilter characterEncodingFilter() {
-        final CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
-        characterEncodingFilter.setEncoding("UTF-8");
-        characterEncodingFilter.setForceEncoding(true);
-        return characterEncodingFilter;
-    }
-
-    @Bean
-    public HttpMessageConverters customConverters() {
-        HttpMessageConverter<String> StringMessageConverter = new StringHttpMessageConverter(Charset.forName("UTF-8"));
-        return new HttpMessageConverters(StringMessageConverter);
-    }
-
-    @Bean
-    public EmbeddedServletContainerFactory servletContainer() {
-        TomcatEmbeddedServletContainerFactory factory = new TomcatEmbeddedServletContainerFactory();
-        factory.setTomcatConnectorCustomizers(Arrays.asList(new TomcatConnectorCustomizer() {
-            @Override
-            public void customize(Connector connector) {
-                connector.setURIEncoding("UTF-8");
-                connector.setUseBodyEncodingForURI(true);
-            }
-        }));
-        return factory;
-    }
-
 }
--- decksheet-api/src/main/java/dragonar/controller/DMSheetController.java
+++ decksheet-api/src/main/java/dragonar/controller/DMSheetController.java
@@ -1,13 +1,15 @@
 package dragonar.controller;
 
 import com.itextpdf.text.DocumentException;
-import com.itextpdf.text.Element;
 import com.itextpdf.text.pdf.BaseFont;
 import com.itextpdf.text.pdf.PdfContentByte;
 import com.itextpdf.text.pdf.PdfReader;
 import com.itextpdf.text.pdf.PdfStamper;
+import dragonar.service.PdfService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
@@ -17,137 +19,30 @@ import javax.servlet.http.*;
 @RequestMapping("/dmSheet")
 public class DMSheetController {
 
-    private static final int LEFT = 80;
-    private static final int RIGHT = 350;
-    private static final int START_MAIN = 655;
-    private static final int START_HYPER = 135;
+    @Autowired
+    private PdfService pdfService;
 
-    @RequestMapping(value = "/", method = RequestMethod.POST)
-    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-
-        // validate
-        if (!validate(request)) {
-            setBadRequestResponse(response);
+    @RequestMapping(value = "", method = RequestMethod.POST)
+    public void doPost(@RequestParam String playerName, @RequestParam String[] mainDeck, @RequestParam(required = false) String[] hyperSpatial, HttpServletResponse res) throws IOException {
+        if (!pdfService.validate(mainDeck, hyperSpatial)) {
+            res.setStatus(400);
             return;
         }
-
-        String playerName = request.getParameter("playerName");
-        String[] mainDeck = request.getParameter("mainDeck").split(",");
-        String[] hyperSpatial = request.getParameter("hyperSpatial").split(",");
-
         try {
             PdfReader reader = new PdfReader("decksheet.pdf");
-            PdfStamper pdfStamper = new PdfStamper(reader, response.getOutputStream());
-
+            PdfStamper pdfStamper = new PdfStamper(reader, res.getOutputStream());
             BaseFont bf = BaseFont.createFont("HeiseiKakuGo-W5", "UniJIS-UCS2-H", BaseFont.NOT_EMBEDDED);
-
             PdfContentByte over = pdfStamper.getOverContent(1);
             over.beginText();
-
-            writeText(over, bf, playerName, 280, 798);
-            writeMainDeck(over, bf, mainDeck);
-            writeHyperSpatial(over, bf, hyperSpatial);
-
+            pdfService.writeText(over, bf, playerName, 280, 798);
+            pdfService.writeMainDeck(over, bf, mainDeck);
+            pdfService.writeHyperSpatial(over, bf, hyperSpatial);
             over.endText();
             pdfStamper.close();
         } catch (DocumentException e) {
             e.printStackTrace();
         }
-        setResponseHeader(response);
-        response.setContentType("application/pdf");
-    }
-
-    private void writeMainDeck(PdfContentByte over, BaseFont bf, String[] mainDeck) {
-        int x = LEFT;
-        int y = START_MAIN;
-        for (int i = 0; i < mainDeck.length; i++) {
-            if (20 <= i) {
-                x = RIGHT;
-            }
-            if (20 == i) {
-                y = START_MAIN;
-            }
-            writeText(over, bf, mainDeck[i], x, y);
-            y -= 22.5;
-        }
-    }
-
-    private void writeHyperSpatial(PdfContentByte over, BaseFont bf, String[] hyperSpatial) {
-        int x = LEFT;
-        int y = START_HYPER;
-        for (int i = 0; i < hyperSpatial.length; i++) {
-            if (4 <= i) {
-                x = RIGHT;
-            }
-            if (4 == i) {
-                y = START_HYPER;
-            }
-            writeText(over, bf, hyperSpatial[i], x, y);
-            y -= 22.5;
-        }
-    }
-
-    private void writeText(PdfContentByte over, BaseFont bf, String text, int x, int y) {
-        over.setFontAndSize(bf, getFontSize(text));
-        over.showTextAligned(Element.ALIGN_TOP, text, x, y, 0);
-    }
-
-    private int getFontSize(String text) {
-        int length = text.length();
-        if (24 < length) {
-            return 8;
-        }
-        if (17 < length) {
-            return 10;
-        }
-        return 12;
-    }
-
-    private boolean validate(HttpServletRequest request) {
-        String mainDeckStr = request.getParameter("mainDeck");
-        String hyperSpatialStr = request.getParameter("hyperSpatial");
-        String playerName = request.getParameter("playerName");
-        if (mainDeckStr == null || mainDeckStr == "" || playerName == null || playerName == "") {
-            return false;
-        }
-        String[] mainDeck = mainDeckStr.split(",");
-        String[] hyperSpatial = hyperSpatialStr.split(",");
-        if (mainDeck.length != 40) {
-            return false;
-        }
-        if (8 < hyperSpatial.length) {
-            return false;
-        }
-        return true;
-    }
-
-    @RequestMapping(value = "/", method = RequestMethod.GET)
-    protected String doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        setResponseHeader(resp);
-        return "";
-    }
-
-    @RequestMapping(value = "/", method = RequestMethod.OPTIONS)
-    protected String doOptions(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        setResponseHeader(resp);
-        return "";
-    }
-
-    private void setBadRequestResponse(HttpServletResponse response) throws IOException {
-        setResponseHeader(response);
-        response.getWriter().println("bad request");
-        response.setStatus(400);
-    }
-
-    private void setResponseHeader(HttpServletResponse response) {
-        response.setHeader("Access-Control-Allow-Origin", "*");
-        response.setHeader("Allow", "*");
-        response.setHeader("Access-Control-Allow-Credentials", "true");
-        response.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
-        response.setHeader("Access-Control-Allow-Headers", "*");
-        response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-        response.setContentType("application/json");
-        response.setStatus(200);
+        res.setContentType("application/pdf");
     }
 
 }
